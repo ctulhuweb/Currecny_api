0120c3e64ed68ec104188408221fca120f4f8b45
ctulhuweb <ctulhuweb@gmail.com>
2020-03-28 13:51:52 +0700

    added rescue from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found(exc)
+    render json: { error: "record not found" }, status: :not_found
+  end
 end
