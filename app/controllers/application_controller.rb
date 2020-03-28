a1007ce4e5687d7c446380c52d65eae144bdd5dd
ctulhuweb <ctulhuweb@gmail.com>
2020-03-28 14:37:01 +0700

    added check authorization token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,30 @@
 class ApplicationController < ActionController::API
+  before_action :authorization
+
+  AUTHORIZATION_TOKEN = "E76dyVeBAiFuudXTYVt4zQXB"
+  
+  NotAuthorized = Class.new(StandardError)
+  
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ApplicationController::NotAuthorized, with: :not_authorized
 
   private
 
   def record_not_found(exc)
     render json: { error: "record not found" }, status: :not_found
+  end
+
+  def not_authorized
+    render json: { error: "Not authorized" }, status: :forbidden
+  end
+
+  def authorization
+    p request.headers["Authorization"]
+    if request.headers["Authorization"].present?
+      token = request.headers["Authorization"].split(" ")[1]
+      raise ApplicationController::NotAuthorized unless token == "E76dyVeBAiFuudXTYVt4zQXB"
+    else
+      raise ApplicationController::NotAuthorized
+    end
   end
 end
